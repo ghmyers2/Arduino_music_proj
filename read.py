6c1de2ee834c5a60e542c9359e90cad7e4580292
shreychowdhary <shrey.chowdhary1@gmail.com>
2019-09-28 17:16:18 -0500

    Decode Serial Data

--- read.py
+++ read.py
@@ -4,6 +4,12 @@ import time
 ser = serial.Serial("/dev/ttyACM0", 115200)
 time.sleep(1)
 
+def decodeSerial(data):
+    decodedStr = data.decode("utf-8").strip()
+    if len(decodedStr) == 0 or decodedStr[0] != "(" or decodedStr[-1] != ")":
+        return None
+    return list(map(float,decodedStr[1:-1].split(",")))
+
 while True:
-    print(ser.readline())
+    print(decodeSerial(ser.readline()))
 
